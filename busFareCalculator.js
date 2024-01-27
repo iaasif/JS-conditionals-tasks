f4859c9b60eabebaf7d9e022fd73f96a081b7f69
iaasif <imtiazasif.cse@gmail.com>
2024-01-27 18:03:40 +0600

    done

--- busFareCalculator.js
+++ busFareCalculator.js
@@ -1,13 +1,10 @@
 /***
-
 Ticket fare Calculator
     - Children (age < 10): free
     - Students get a 50% discount
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
-
-
 let youAre = "Students";
 let age = 110;
 youAre == "Children" && age < 10 ? console.log("free") : youAre == "Students" ? console.log("get 50% discount") : youAre == "Senior" && age >= 60 ? console.log("get 15% discount") : console.log("regular fare");
--- gradeCalculator.js
+++ gradeCalculator.js
@@ -1,5 +1,4 @@
 /***
-
 Grade Calculator
 
 Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
@@ -14,4 +13,14 @@ Create a simple JavaScript program that takes a student's score as input and ret
 
 let result = 78;
 
-result >= 90 && result <= 100 ? console.log("A") : result >= 80 && result <= 89 ? console.log("B") : result >= 70 && result <= 79 ? console.log("C") : result >= 60 && result <= 69 ? console.log("D") : result >= 0 && result <= 59 ? console.log("F") : console.log("out of range");
+result >= 90 && result <= 100 ?
+    console.log("A") :
+    result >= 80 && result <= 89 ?
+        console.log("B") :
+        result >= 70 && result <= 79 ?
+            console.log("C") :
+            result >= 60 && result <= 69 ?
+                console.log("D") :
+                result >= 0 && result <= 59 ?
+                    console.log("F") :
+                    console.log("out of range");
