ba5d62a6b8d4f8e369bf00b58d281edb66006477
iaasif <imtiazasif.cse@gmail.com>
2024-01-27 15:02:36 +0600

    one problem solved and all name changed

---
+++ freeDrinks.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 600;
+
+if(burgerPrice>500)
+{
+    console.log("get free coke");
+}
+else{
+    console.log("coke price 30tk");
+}
